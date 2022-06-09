e78bdb3457b027e4a4bf7d51b0c78b2a4e1b1a90
Sofia Anjos <emmanuelesofia@gmail.com>
2022-06-08 21:24:46 -0300

    fiz as ultimas alterações

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import styled from 'styled-components'
-import Post from './components/Post/Post';
+import React from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 class App extends React.Component {
   render() {
     return (
       <MainContainer>
         <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
+          nomeUsuario={"paulinha"}
+          fotoUsuario={"https://picsum.photos/50/50"}
+          fotoPost={"https://picsum.photos/200/150"}
+        />
+
+        <Post
+          nomeUsuario={"juju"}
+          fotoUsuario={
+            "https://i.pinimg.com/originals/8e/60/39/8e6039a4c9cb6553c2bfcdb042ce8f21.jpg"
+          }
+          fotoPost={
+            "https://i.pinimg.com/736x/86/5c/40/865c40a5a12fba846c9042003409c5d0.jpg"
+          }
+        />
+
+        <Post
+          nomeUsuario={"lulu"}
+          fotoUsuario={
+            "https://i.pinimg.com/originals/6f/9b/24/6f9b24e85d5bfb8acff726b5457bbd5c.jpg"
+          }
+          fotoPost={
+            "https://i.pinimg.com/originals/47/02/7e/47027ea0b972f76891551f5526f4d452.jpg"
+          }
         />
       </MainContainer>
     );
