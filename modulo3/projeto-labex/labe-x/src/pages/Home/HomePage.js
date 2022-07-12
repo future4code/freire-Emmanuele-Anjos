a0f29d3980a7529cafaf6a956c079f7e1fae211d
Sofia Anjos <emmanuelesofia@gmail.com>
2022-07-12 20:44:15 -0300

    adicionei as routers

---
+++ modulo3/projeto-labex/labe-x/src/pages/Home/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Bem vindo ao labeX</h1>
+    </div>
+  );
+};
