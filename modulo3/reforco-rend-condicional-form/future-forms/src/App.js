7b78609df4dd819978f6309e6ff1072fb5901240
Sofia Anjos <emmanuelesofia@gmail.com>
2022-08-02 00:01:19 -0300

    logica finalizada

--- modulo3/reforco-rend-condicional-form/future-forms/src/App.js
+++ modulo3/reforco-rend-condicional-form/future-forms/src/App.js
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
-import "./App.css";
 import Etapa1 from "./Components/Etapa1";
+import Etapa2 from "./Components/Etapa2";
+import Etapa3 from "./Components/Etapa3";
+import LastPage from "./Components/LastPage";
+import styled from "styled-components";
+
+// const Container = styled.div`
+//   text-align: center;
+// `;
 
 function App() {
-  const [etapa, setEtapa] = useState();
+  const [etapa, setEtapa] = useState(1);
 
   const renderizaTela = () => {
     switch (etapa) {
       case 1:
         return <Etapa1 Etapa2={setEtapa} />;
       case 2:
-        return <Etapa2 />;
+        return <Etapa2 Etapa3={setEtapa} />;
       case 3:
-        <Etapa3 />;
+        return <Etapa3 LastPage={setEtapa} />;
+      case 4:
+        return <LastPage />;
       default:
-        return <p>default</p>;
+        return <LastPage />;
     }
-    return <div>{rendenrizaTela()}</div>;
   };
+
+  const trocarTela = (tela) => {
+    setEtapa(tela + 1);
+  };
+
+  return (
+    <div>
+      {renderizaTela()}
+      <button onClick={() => trocarTela(etapa)}>Próxima Etapa</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ modulo3/reforco-rend-condicional-form/future-forms/src/Components/Etapa2.js
@@ -0,0 +1,13 @@
+function Etapa2(props) {
+  return (
+    <div>
+      <h1>Etapa 2: Informações do Ensino Superior</h1>
+      <p>5. Qual curso?</p>
+      <input />
+      <p>6. Qual a unidade de ensino?</p>
+      <input />
+    </div>
+  );
+}
+
+export default Etapa2;
