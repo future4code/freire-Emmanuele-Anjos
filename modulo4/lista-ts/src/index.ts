00ff764f4c594cad3059a1919e9eb8cd07f65e5a
Sofia Anjos <emmanuelesofia@gmail.com>
2022-08-14 20:35:03 -0300

    exercício 3

--- modulo4/lista-ts/src/index.ts
+++ modulo4/lista-ts/src/index.ts
@@ -25,3 +25,31 @@ const type3 = 333;
 randomType(type1);
 randomType(type2);
 randomType(type3);
+
+//exercício 3
+
+enum GENERO {
+  ACAO = "ação",
+  DRAMA = "drama",
+  COMEDIA = "comédia",
+  ROMANCE = "romance",
+  TERROR = "terror",
+}
+
+type setMovies = {
+  name: string;
+  year: number;
+  gender: string;
+};
+
+const printCatalogue = (
+  name: string,
+  releaseYear: number,
+  movieGender: string
+): object => {
+  return { name: name, releaseYear: releaseYear, movieGender: movieGender };
+};
+console.log(printCatalogue("A Brighter Summer Day", 1991, GENERO.DRAMA));
+
+/* ex Entradas: "Duna", 2021, GENERO.ACAO
+Saída: {nome: "Duna", anoLancamento: 2021, genero: "ação"}*/
